c37a6a7893b8c98923f8fc4d76f2a716351f1000
Jaime <jaimegarciamendez@gmail.com>
2019-08-23 14:21:32 -0300

    resource internment

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :patient do
-    lastname = "X"
-    firstname = "Paciente"
-    document_type = "DU"
-    document_number = "31060702"
-    gender = "Trans"
-    birth_date = "30/04/1980"
+  factory :patient, class: Patient do
+    firstname { FFaker::Name.first_name }
+    lastname { FFaker::Name.last_name }
+    document_number { FFaker::Identification.ssn }
+    association :clinic, strategy: :build
+    clinic_id {clinic.id}
   end
 end
