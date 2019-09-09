1e9fdc5e3f8a049facc7fe5f412b97be6a718d2c
Jaime <jaimegarciamendez@gmail.com>
2019-09-09 17:44:05 -0300

    test search clinic not working

--- spec/services/search/clinic.rb
+++ spec/services/search/clinic.rb
@@ -3,32 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe ClinicsController, type: :controller do
-  context 'Get clinics#index' do
-    @clinic = FactoryBot.create(:clinic)
-    let!(:user) { FactoryBot.create(:user) }
-    let!(:user1) { FactoryBot.create(:user, clinic_id: @clinic) }
-
-    @clinic = FactoryBot.create(:clinic)
+  context 'Search Tests' do
+    clinic = FactoryBot.create_list(:clinic, 3)
+    let!(:user) { FactoryBot.create(:user, clinic_id: nil) }
+    let!(:user1) { FactoryBot.create(:user, clinic_id: clinic) }
     params = {}
 
-    it 'Shows all the clinics' do
+    it 'Shows all the Clinics if User.clinic_id.nil' do
       service = Search::Clinic.new(user,params)
       service.run
       service.data
-      expect(response).to have_http_status(:success)
+      expect(service.data).to eq(3) 
     end
 
-    it 'search only one clinic' do
+    it 'Shows only one Clinic if User.clinic_id not nil' do
       service = Search::Clinic.new(user1,params)
-      service.run
-      expect(service.data).to eq(@clinic)
-      expect(response).to have_http_status(:success)
+      expect(service.data) == clinic
     end
-
-    it 'Responds to JSON' do
-      get :index, format: :json
-      expect(response.content_type).to eq 'application/json; charset=utf-8'
-    end
-
   end
 end
