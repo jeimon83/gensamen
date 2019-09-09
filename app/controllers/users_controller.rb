9362c4178492fd283995a644d7e5e6f9d847d5cb
Jaime <jaimegarciamendez@gmail.com>
2019-09-09 15:33:34 -0300

    add user controller update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,30 @@
 
 # Users Controller
 class UsersController < ApplicationController
+  before_action :set_user, only: [:update]
+
   def index
-    service = Search::User.new(current_user, params)
+    service = Search::User.new(params)
     service.run
-    render json: service.data, each_serializer: UserSerializer, meta: service.metadata
+    users = service.data.map { |user| { id: user.id, email: user.email, name: user.full_name } }
+    render json: { users: users, meta: service.metadata }
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user, serializer: UserSerializer
+    else
+      render json: @clinic.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :phone, :role, :password, :clinic_id)
   end
 end
