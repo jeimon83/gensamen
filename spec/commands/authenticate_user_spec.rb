9c674d9fff9980afac44affa21e7fdf2fc6a0acd
Jaime <jaimegarciamendez@gmail.com>
2019-09-09 10:57:29 -0300

    test auth user not working

--- spec/commands/authenticate_user_spec.rb
+++ spec/commands/authenticate_user_spec.rb
@@ -4,21 +4,17 @@ require 'rails_helper'
 
 describe AuthenticateUser do
   subject(:context) { described_class.call(email, password) }
-
   describe '.call' do
-    context 'when the context is successful' do
+    context 'when auth is successful' do
       let(:email) { 'correct_email' } 
       let(:password) { 'correct_password' }
-      
       it 'succeeds' do
         expect(context).to be_success
       end
     end
-
     context 'when the context is not successful' do
       let(:email) { 'wrong_email' }
       let(:password) { 'wrong_password' }
-
       it 'fails' do
         expect(context).to be_failure
       end
