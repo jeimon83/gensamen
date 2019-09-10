a3a851f04447ae0952ca70860f34db1ab03df902
Jaime <jaimegarciamendez@gmail.com>
2019-09-10 17:25:14 -0300

    create report request table

---
+++ db/migrate/20190910192757_create_report_requests.rb
@@ -0,0 +1,14 @@
+class CreateReportRequests < ActiveRecord::Migration[6.0]
+  def change
+    create_table :report_requests do |t|
+      t.references :clinic, null: false, foreign_key: true
+      t.references :patient, null: false, foreign_key: true
+      t.date :date
+      t.string :type
+      t.date :expiration_date
+      t.string :answer
+
+      t.timestamps
+    end
+  end
+end
