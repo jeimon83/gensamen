857d2e2b2cb1077945173570c268cbc7d51a700e
Jaime <jaimegarciamendez@gmail.com>
2019-09-10 17:26:51 -0300

    report request serializer

---
+++ app/serializers/report_request_serializer.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: report_requests
+#
+#  id              :bigint           not null, primary key
+#  clinic_id       :bigint           not null
+#  patient_id      :bigint           not null
+#  date            :date
+#  type            :string
+#  expiration_date :date
+#  answer          :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# ReportRequest Serializer
+class ReportRequestSerializer < ActiveModel::Serializer
+  attributes :id, :date, :type, :expiration_date, :answer
+  belongs_to :clinic
+  belongs_to :patient, optional: true
+end
