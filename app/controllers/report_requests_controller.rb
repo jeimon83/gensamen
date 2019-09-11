96fda61f604d0f91278b5c8d0b3cb5ad06b4c410
Jaime <jaimegarciamendez@gmail.com>
2019-09-10 23:17:23 -0300

    report request controller

---
+++ app/controllers/report_requests_controller.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+# Report Request Controller
+class ReportRequestsController < ApplicationController
+  before_action :set_report_request, only: [:show, :update, :destroy]
+
+  def index
+    @report_requests = ReportRequest.all
+    render json: @report_requests, each_serializer: ReportRequestSerializer
+  end
+
+  def create
+    @report_request = ReportRequest.new(report_request_params)
+    if @report_request.save
+      render json: @report_request, serializer: ReportRequestSerializer
+    else
+      render json: @report_request.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @report_request, serializer: ReportRequestSerializer
+  end
+
+  def update
+    if @report_request.update(report_request_params)
+      render json: @report_request, serializer: ReportRequestSerializer
+    else
+      render json: @report_request.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @report_request.destroy
+    if @report_request.destroyed?
+      render json: {}, status: :no_content
+    else
+      render json: @report_request.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_clinic
+    @clinic = Clinic.find(params[:clinic_id])
+  end
+
+  def set_report_request
+    @report_request = ReportRequest.find(params[:id])
+  end
+
+  def report_request_params
+    params.require(:report_request).permit(:clinic_id, :patient_id, :date, :type, :expiration_date, :answer, documents: [])
+  end
+end
---
+++ spec/models/report_request_spec.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: report_requests
+#
+#  id              :bigint           not null, primary key
+#  clinic_id       :bigint           not null
+#  patient_id      :bigint           not null
+#  date            :date
+#  type            :string
+#  expiration_date :date
+#  answer          :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe ReportRequest, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
