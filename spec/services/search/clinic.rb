3832c4e950e339c63f383359a76b29217727c88b
Jaime <jaimegarciamendez@gmail.com>
2019-09-12 17:29:07 -0300

    improve search clinic test

--- spec/services/search/clinic.rb
+++ spec/services/search/clinic.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 
 #RSpec.describe ClinicsController, type: :controller do
 RSpec.describe Search::Clinic do
-  context 'Search' do
+  context 'Service' do
     let!(:clinic) { FactoryBot.create(:clinic) }
-    let!(:user) { FactoryBot.create(:user) }
+    let!(:user) { FactoryBot.create(:user, clinic_id: clinic.id) }
     params = {}
     it 'Search all the Clinics' do
       user.clinic_id = nil
@@ -16,11 +16,16 @@ RSpec.describe Search::Clinic do
       expect(service.data).to contain_exactly(*Clinic.all)
     end
     it 'Shows only one Clinic' do
-      user.clinic_id = clinic.id
       service = Search::Clinic.new(user,params)
       expect(service.fetch_data.count).to eq(1)
       expect(service.filter_data).to eq(nil)
       expect(service.data).to contain_exactly(*Clinic.where(id: user.clinic_id))
+    end
+    it 'Returns nothing' do
+      user.clinic_id = Clinic.last.id + 1
+      service = Search::Clinic.new(user,params)
+      puts service.fetch_data.count
+      expect(service.fetch_data.count).to eq(0)
     end
   end
 end
