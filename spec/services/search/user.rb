b943ecf69552e0ac9b2a3b23e312da0fb8465efc
Jaime <jaimegarciamendez@gmail.com>
2019-09-13 11:14:45 -0300

    test user search service

--- spec/services/search/user.rb
+++ spec/services/search/user.rb
@@ -17,9 +17,18 @@ RSpec.describe Search::User do
       expect(service.data).to contain_exactly(*x)
     end
     it 'Shows only one User' do
+      x = user.clinic_id
+      puts x
+      puts user.clinic_id
+      user = User.find_by(clinic_id: x)
+      puts user
       service = Search::User.new(user,params)
       expect(service.fetch_data.count).to eq(1)
       expect(service.filter_data).to eq(nil)
+      expect(service.fetch_data).not_to be_empty
+      #expect(service.fetch_data).to equal(user)
+      #expected = service.fetch_data
+      #expect(user).to eq(expected)
     end
   end
 end
