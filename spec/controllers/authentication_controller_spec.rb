b4bcb31e4f9f3034ec1c28e3401de91bf6f0240e
Jaime <jaimegarciamendez@gmail.com>
2019-09-13 11:38:53 -0300

    improve auth controller test

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -5,14 +5,17 @@ require 'rails_helper'
 
 RSpec.describe AuthenticationController, type: :controller do
   context 'Login with Authenticacion Header' do
-    let!(:user) { FactoryBot.create(:user) }
+    let!(:user) { FactoryBot.create(:user, email: "mymail@email.com", password: "123qwe") }
     it 'Returns Auth Token with User' do
       post :authenticate, params: {"auth": {"email": user.email, "password": user.password }}
       expect(response.body['user']).to be_present
       expect(response.body['auth_token']).to be_present
       expect(response).to have_http_status(:success)
-      parsed_body = JSON.parse(response.body)
-      expect(parsed_body.keys).to match_array(["auth_token", "user"])
+    end
+    it 'Returns an invalid credentials error' do
+      post :authenticate, params: {"auth": {"email": "myemail@mail.com", "password": "cualquiercosa" }}
+      expect(response.body).to match("Error con las credenciales, verifique email y contraseña")
+      expect(response).to have_http_status(401)
     end
   end
 end
