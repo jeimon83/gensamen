fe6ea09461908b6810430e54064e833694169511
Jaime <jaimegarciamendez@gmail.com>
2019-09-15 07:24:12 -0300

    describe block & method and add contexts

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -4,18 +4,22 @@
 require 'rails_helper'
 
 RSpec.describe AuthenticationController, type: :controller do
-  context 'Login with Authenticacion Header' do
-    let!(:user) { FactoryBot.create(:user, email: "mymail@email.com", password: "123qwe") }
-    it 'Returns Auth Token with User' do
-      post :authenticate, params: {"auth": {"email": user.email, "password": user.password }}
-      expect(response.body['user']).to be_present
-      expect(response.body['auth_token']).to be_present
-      expect(response).to have_http_status(:success)
+  describe '.authenticate' do
+    context 'when user login with valid email & password' do
+      let!(:user) { FactoryBot.create(:user, email: "mymail@email.com", password: "123qwe") }
+      it 'returns auth token & user' do
+        post :authenticate, params: {"auth": {"email": user.email, "password": user.password }}
+        expect(response.body['user']).to be_present
+        expect(response.body['auth_token']).to be_present
+        expect(response).to have_http_status(:success)
+      end
     end
-    it 'Returns an invalid credentials error' do
-      post :authenticate, params: {"auth": {"email": "myemail@mail.com", "password": "cualquiercosa" }}
-      expect(response.body).to match("Error con las credenciales, verifique email y contraseña")
-      expect(response).to have_http_status(401)
+    context 'when user login with invalid mail or password' do
+      it 'returns an invalid credentials error' do
+        post :authenticate, params: {"auth": {"email": "myemail@mail.com", "password": "cualquiercosa" }}
+        expect(response.body).to match("Error con las credenciales, verifique email y contraseña")
+        expect(response).to have_http_status(401)
+      end
     end
   end
 end
