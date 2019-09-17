0ff3cdeec404c680b07a26292bbc27a1321d4415
Jaime <jaimegarciamendez@gmail.com>
2019-09-17 11:11:17 -0300

    improve patient & user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,41 +19,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'Do validation tests' do
-    let!(:user) { build(:user) }
-    it 'Ensures first name presence' do
-      user.first_name = nil
-      expect(user.valid?).to eq(false)
+  let(:subject) { described_class.new(email: 'a@a.com', first_name: 'A', last_name: 'B', password: '123qwe') }
+  
+  describe 'model attributes' do
+    context 'validation tests' do
+      it 'ensures first name presence' do
+        subject.first_name = nil
+        expect(subject.valid?).to eq(false)
+      end
+      it 'ensures last name presence' do
+        subject.last_name = nil
+        expect(subject.valid?).to eq(false)
+      end
+      it 'ensures email presence' do
+        subject.email = nil
+        expect(subject.valid?).to eq(false)
+      end
+      it 'ensures password presence' do
+        subject.password = nil
+        expect(subject.valid?).to eq(false)
+      end
+      it 'saves successfully' do
+        expect(subject.save).to eq(true)
+      end
     end
-    it 'Ensures last name presence' do
-      user.last_name = nil
-      expect(user.valid?).to eq(false)
+    context 'test full_name method' do
+      it 'should say full name' do
+        expect(subject.full_name).to eq "#{subject.first_name} #{subject.last_name}"
+      end
     end
-    it 'Ensures email presence' do
-      user.email = nil
-      expect(user.valid?).to eq(false)
-    end
-    it 'Ensures password presence' do
-      user.password = nil
-      expect(user.valid?).to eq(false)
-    end
-    it 'Saves successfully' do
-      expect(user.save).to eq(true)
-    end
-  end
-  context 'Test Full_Name method' do
-    let(:user) { FactoryBot.build(:user, first_name: 'Jaime', last_name: 'GM') }
-    it 'Should say: "Jaime GM"' do
-      expect(user.full_name).to eq 'Jaime GM'
-    end
-  end
-  context 'Testing Clinic relation' do
-    it 'Belongs to a Clinic' do
-      expect { FactoryBot.build(:user).clinic }.to_not raise_error
-    end
-    it 'Belongs to Clinic Test Nº2' do
-      assc = User.reflect_on_association(:clinic)
-      expect(assc.macro).to eq :belongs_to
+    context 'testing relations' do
+      it 'belongs to clinic test nº1' do
+        expect { FactoryBot.build(:user).clinic }.to_not raise_error
+      end
+      it 'belongs to clinic test nº2' do
+        assc = User.reflect_on_association(:clinic)
+        expect(assc.macro).to eq :belongs_to
+      end
     end
   end
 end
