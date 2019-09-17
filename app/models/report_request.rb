7db6cd631b34894148df09303d749391653e7955
Jaime <jaimegarciamendez@gmail.com>
2019-09-17 16:31:36 -0300

    validations

--- app/models/report_request.rb
+++ app/models/report_request.rb
@@ -1,4 +1,20 @@
+# == Schema Information
+#
+# Table name: report_requests
+#
+#  id              :bigint           not null, primary key
+#  clinic_id       :bigint           not null
+#  patient_id      :bigint           not null
+#  requested_date  :date
+#  type            :string
+#  expiration_date :date
+#  answer          :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class ReportRequest < ApplicationRecord
+  validates :requested_date, :type, presence: true
   belongs_to :clinic
   belongs_to :patient, optional: true
 
