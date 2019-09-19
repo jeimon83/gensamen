aa5a8274e6c39f4b0105350aee2315a64d446d54
Jaime <jaimegarciamendez@gmail.com>
2019-09-19 10:23:58 -0300

    change user profile routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,9 @@ Rails.application.routes.draw do
   get  '/check', to: 'application#service'
   post '/login', to: 'authentication#authenticate'
   
-  resources :users, only: [:index] do
-    member do
-      get   '/profile',   to: 'users#profile'
-      patch '/profile',   to: 'users#update_profile'
-    end
-  end
+  resources :users, only: [:index]
+  get   '/profile', to: 'users#profile',        as: 'profile'
+  patch '/profile', to: 'users#update_profile', as: 'edit_profile'
       
   resources :clinics, shallow: true do
     resources :patients do
