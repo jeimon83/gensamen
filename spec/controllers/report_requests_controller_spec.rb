3acd20b839e4fb0f8644e64c2abba9215aad2e4b
Jaime <jaimegarciamendez@gmail.com>
2019-09-20 07:47:24 -0300

    rspec

--- spec/controllers/report_requests_controller_spec.rb
+++ spec/controllers/report_requests_controller_spec.rb
@@ -3,58 +3,44 @@
 require 'rails_helper'
 
 RSpec.describe ReportRequestsController, type: :controller do
+  let!(:user) { FactoryBot.create(:user) }
   let!(:clinic) { FactoryBot.create(:clinic) }
-  let!(:other_clinic) { FactoryBot.create(:clinic) }
   let!(:patient) { FactoryBot.create(:patient) }
-  let!(:admin_user) { FactoryBot.create(:user, :admin) }
-  let!(:common_user) { FactoryBot.create(:user, clinic_id: other_clinic.id) }
-  
+  let!(:report_request) { FactoryBot.create(:report_request, clinic_id: clinic.id, patient_id: patient.id) }
+
   describe 'GET #index' do
-    context 'when user is admim' do   
-      it 'returns a success response' do
-        allow(AuthorizeApiRequest).to receive_message_chain(:call, :result).and_return(admin_user)
+    context 'when user is authorized' do
+      it 'returns an http success response' do
+        allow(AuthorizeApiRequest).to receive_message_chain(:call, :result).and_return(user)
         get :index, params: { clinic_id: clinic.id }
+        expect(response.body['report_request']).to be_present
         expect(response).to have_http_status(:success)
       end
     end
-    context 'when user is from other clinic' do
+    context 'when authorize api request fails' do
       it 'returns a failure response' do
-        allow(AuthorizeApiRequest).to receive_message_chain(:call, :result).and_return(common_user)
+        allow(AuthorizeApiRequest).to receive_message_chain(:call, :result).and_return(nil)
         get :index, params: { clinic_id: clinic.id }
         expect(response).to have_http_status(401)
-      end
-    end
-    context 'format view' do
-      it 'responds to json' do
-        get :index, format: :json, params: { clinic_id: clinic.id }
-        expect(response.content_type).to eq 'application/json; charset=utf-8'
       end
     end
   end
 
   describe 'GET #show' do
-    context 'when user is admin' do
-      it 'renders the report request' do
-        allow(AuthorizeApiRequest).to receive_message_chain(:call, :result).and_return(admin_user)
-        get :show, params: { id: patient.id }
-        expect(response.body['report_request']).to be_present
-        expect(response.content_type).to eq 'application/json; charset=utf-8'
-      end
-    end
-    context 'when user is from other clinic' do
-      it 'renders error: not authorized' do
-        allow(AuthorizeApiRequest).to receive_message_chain(:call, :result).and_return(common_user)
+    context 'when user is authorized' do
+      it 'renders the report definition' do
+        allow(AuthorizeApiRequest).to receive_message_chain(:call, :result).and_return(user)
         get :show, params: { id: report_request.id }
-        expect(response.body).to match("Not Authorized")
+        expect(response.body['report_request']).to be_present    
       end
     end
   end
 
   describe 'PATCH #update' do
-    context 'when user is admin' do
+    context 'when user is authorized' do
       it 'updates the report request' do
-        allow(AuthorizeApiRequest).to receive_message_chain(:call, :result).and_return(admin_user)
-        patch :update, params: { id: report_request.id, report_request: { requested_date: '20-03-2020' } }
+        allow(AuthorizeApiRequest).to receive_message_chain(:call, :result).and_return(user)
+        patch :update, params: { id: report_request.id, report_request: { answer: 'Respuesta' } }
         expect(response.body['report_request']).to be_present
         expect(response).to have_http_status(:success)
       end
@@ -62,17 +48,10 @@ RSpec.describe ReportRequestsController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
-    context 'when user is admin' do
-      it 'destroys the report request' do
-        allow(AuthorizeApiRequest).to receive_message_chain(:call, :result).and_return(admin_user)
-        expect { delete :destroy, params: { id: report_request }}.to change { ReportRequest.count }.by(-1)
-      end
-    end
-    context 'when user is from other clinic' do
-      it 'renders error: not authorized' do
-        allow(AuthorizeApiRequest).to receive_message_chain(:call, :result).and_return(common_user)
-        delete :destroy, params: { id: report_request.id }
-        expect(response.body).to match("Not Authorized")
+    context 'when user is authorized' do
+      it 'destroys the report_request' do
+        allow(AuthorizeApiRequest).to receive_message_chain(:call, :result).and_return(user)
+        expect { delete :destroy, params: { id: report_request } }.to change { ReportRequest.count }.by(-1)
       end
     end
   end
