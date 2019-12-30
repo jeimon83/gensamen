1a302b9c7951db8911a6a5f1a8eca40fff500efc
Jaime <jaimegarciamendez@gmail.com>
2019-12-30 14:57:16 -0300

    remove migration file

--- db/migrate/20191230172919_add_title_and_description_to_help_requests.rb
+++
@@ -1,6 +0,0 @@
-class AddTitleAndDescriptionToHelpRequests < ActiveRecord::Migration[6.0]
-  def change
-    add_column :help_requests, :text, :string
-    add_column :help_requests, :description, :text
-  end
-end
